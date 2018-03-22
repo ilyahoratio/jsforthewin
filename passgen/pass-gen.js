a5524b6969550fa4b2b58fc47795521fcd9fde72
Ilya Horatio <ilyahoratio@gmail.com>
2018-03-22 16:16:11 +0300

    added a readme file + the file itself

---
+++ passgen/pass-gen.js
@@ -0,0 +1,28 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Set the password length: ", function (passwordLength) {
+  console.log("The password length will be:", passwordLength);
+  console.log("Wait for it...");
+  console.log("----------");
+
+  rl.close();
+
+  var passwordChars = "AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!?@/|)(%#}{][+-=:;"
+  var password = '';
+
+  var newChar = 0;
+
+  while (password.length < (passwordLength)) {
+    var passwordCharsIndex = Math.floor(Math.random() * (passwordChars.length));
+    password += passwordChars[passwordCharsIndex];
+    newChar++;
+  }
+
+  console.log(`Here's your randomly ${passwordLength} symbols generated password:
+${password}`);
+});
---
+++ passgen/readme.md
@@ -0,0 +1,7 @@
+Just fooling around with js and node.js
+
+Run the file through node 
+
+Select the length of your password
+
+Get a randomly generated password with the length you had chosen
